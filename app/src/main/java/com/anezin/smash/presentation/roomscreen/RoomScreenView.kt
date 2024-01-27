d93ff16c00d2e187ebc4423a07f16b05c2d53656
Lucas <lucas.anezin@gmail.com>
2024-01-27 15:06:01 -0300

    Empezado con roomscreen view

--- app/src/main/java/com/anezin/smash/presentation/roomscreen/RoomScreenView.kt
+++ app/src/main/java/com/anezin/smash/presentation/roomscreen/RoomScreenView.kt
@@ -1,23 +1,53 @@
 package com.anezin.smash.presentation.roomscreen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.anezin.smash.infrastructure.factories.Factory
+import kotlinx.coroutines.launch
 
 class RoomScreenView {
 
+    @Preview(showSystemUi = true)
     @Composable
-    fun Build(navController: NavController, viewModel: RoomScreenViewModel = Factory.roomScreenViewModel) {
+    fun Preview() {
+        Content(playerCount = 1)
+    }
+
+    @Composable
+    fun Build(
+        navController: NavController,
+        viewModel: RoomScreenViewModel = Factory.roomScreenViewModel
+    ) {
         viewModel.initializeViewModel()
         val foundRoomState by viewModel.uiState.collectAsState()
 
-        Column {
-            Text("Cantidad de gente en la sala: ${foundRoomState.room?.players?.count()}")
+        Content(foundRoomState.room?.players?.count())
+    }
 
+    @Composable
+    private fun Content(playerCount: Int?) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Cantidad de gente en la sala: ${playerCount}/4")
+            Button(onClick = {
+            }) {
+                Text(text = "Empezar partida")
+            }
         }
     }
 }
